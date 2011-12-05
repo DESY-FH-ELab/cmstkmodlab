07d818191b0fcfd14eedc10a3f6d991ad624ae57
olzem <olzem>
2011-12-05 16:11:25 +0000

    * forgot to add DefoCamHandler

---
+++ defo/DefoCamHandler.cc
@@ -0,0 +1,33 @@
+
+
+#include "DefoCamHandler.h"
+
+
+///
+/// when calling this, make sure that
+/// action_ and - if necessary - cfg_ have
+/// been set properly beforehand
+///
+void DefoCamHandler::run( void ) {
+
+  switch( action_ ) {
+
+  case DefoCamHandler::GETCFG:
+    // nothing yet
+    break;
+
+  case DefoCamHandler::SETCFG:
+    // nothing yet
+    break;
+
+  case DefoCamHandler::GETIMAGE:
+    camera_.getAndSaveImage( filePath_ );
+    break;
+
+  default:
+    std::cerr << " [DefoCamHandler::run] ** ERROR: switch reached default" << std::endl;
+    throw;
+
+  }
+
+}
---
+++ defo/DefoCamHandler.h
@@ -0,0 +1,41 @@
+
+
+#ifndef _DEFOCAMHANDLER_H
+#define _DEFOCAMHANDLER_H
+
+#include <string>
+
+#include <QThread>
+
+#include "EOS550DSyscall.h"
+
+
+///
+/// threaded for handling cam access
+///
+class DefoCamHandler : public QThread {
+
+ Q_OBJECT
+ 
+ public:
+
+  enum Action { GETCFG, SETCFG, GETIMAGE, INVALID };
+
+  DefoCamHandler() {}
+  ~DefoCamHandler() {}
+  void setAction( DefoCamHandler::Action a ) { action_ = a; }
+  void setCfg( EOS550D::EOS550DConfig const& c ) { cfg_ = c; }
+  void setFilePath( std::string const& p ) { filePath_ = p; }
+
+ private:
+  EOS550DSyscall camera_;
+  std::string filePath_;
+  DefoCamHandler::Action action_;
+  EOS550D::EOS550DConfig cfg_;
+  
+ protected:
+  void run();
+
+};
+
+#endif
