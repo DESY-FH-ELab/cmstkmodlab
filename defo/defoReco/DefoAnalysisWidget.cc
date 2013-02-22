ee47911460dd1b674a2a01759cf716bb2c3c7bdb
Negusbuk <andreas@mussgiller.de>
2013-02-22 14:00:47 +0000

    disable fancy 3D plot (for now)

--- defo/defoReco/DefoAnalysisWidget.cc
+++ defo/defoReco/DefoAnalysisWidget.cc
@@ -22,20 +22,19 @@ DefoAnalysisWidget::DefoAnalysisWidget(
   plot3D_->setMinimumSize(QSize(400, 400));
   plot3D_->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
 
-  plot3D_->setRotation(30,0,15);
-  plot3D_->setScale(1,1,1);
-  plot3D_->setShift(0.15,0,0);
-  plot3D_->setZoom(0.9);
+//  plot3D_->setRotation(30,0,15);
+//  plot3D_->setScale(1,1,1);
+//  plot3D_->setShift(0.15,0,0);
+//  plot3D_->setZoom(0.9);
 
-  plot3D_->setCoordinateStyle(Qwt3D::BOX);
-  plot3D_->updateData();
-  plot3D_->updateGL();
+//  plot3D_->setCoordinateStyle(Qwt3D::BOX);
+//  plot3D_->updateData();
+//  plot3D_->updateGL();
 
   layout->addWidget(plot3D_);
-  updateGeometry();
 
-  connect(selectionModel_, SIGNAL(selectionChanged(DefoMeasurementPair*)),
-          this, SLOT(selectionChanged(DefoMeasurementPair*)));
+  //connect(selectionModel_, SIGNAL(selectionChanged(DefoMeasurementPair*)),
+  //        this, SLOT(selectionChanged(DefoMeasurementPair*)));
 }
 
 void DefoAnalysisWidget::selectionChanged(DefoMeasurementPair* selection) {
@@ -43,6 +42,10 @@ void DefoAnalysisWidget::selectionChanged(DefoMeasurementPair* selection) {
   std::cout << "selectionChanged" << std::endl;
 
   const DefoSurface& surface = selection->getSurface();
+  std::cout << "selectionChanged" << std::endl;
   plot3D_->setData(surface);
+  std::cout << "selectionChanged" << std::endl;
   plot3D_->draw();
+
+  std::cout << "selectionChanged" << std::endl;
 }
