0bd442e755cb2394152dd56d76390be3539f4bd4
Negusbuk <andreas@mussgiller.de>
2013-07-22 07:15:02 +0000

    fix for new version of Pfeiffer TPG262 interface classes

--- tools/ReadTPG262/ReadTPG262.cpp
+++ tools/ReadTPG262/ReadTPG262.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -7,45 +8,50 @@
 
 #ifdef USE_FAKEIO
 #include "PfeifferTPG262Fake.h"
+typedef PfeifferTPG262Fake PfeifferTPG262_t;
 #else
 #include "PfeifferTPG262.h"
+typedef PfeifferTPG262 PfeifferTPG262_t;
 #endif
 
 int main(int argc, char * argv[])
 {
-  if (argc>2) {
-    std::cout << "usage: ReadTPG262 [<filename>]" << std::endl;
+  if (argc>3) {
+    std::cout << "\nusage: ReadTPG262 [<filename>]\n" << std::endl;
   }
   
   VPfeifferTPG262 * tpg262;  
-#ifdef USE_FAKEIO
-  tpg262 = new PfeifferTPG262Fake("/dev/ttyS4");
-#else
-  tpg262 = new PfeifferTPG262("/dev/ttyS4");
-#endif
 
-  double p1, p2;
-  int errorStatus = tpg262->GetErrorStatus();
+  tpg262 = new PfeifferTPG262_t("/dev/ttyS4");
 
-  if (argc==1) {
-    std::cout.precision(3);
-    std::cout.setf(std::ios::fixed, std::ios::floatfield);
-    std::cout << p1 << std::endl;
-    std::ofstream ofile("ReadTPG262.last");
-    ofile.precision(3);
-    ofile.setf(std::ios::fixed, std::ios::floatfield);
-    ofile << p1 << std::endl;
-  } else {
-    std::ofstream ofile(argv[1], std::fstream::app);
-    if (ofile.good()) {
-      time_t seconds = time(NULL);
-      ofile << seconds << "   ";
-      ofile.precision(3);
-      ofile.setf(std::ios::fixed, std::ios::floatfield);
-      ofile << p1 << std::endl;
+  VPfeifferTPG262::reading_t p1, p2;
+
+  while(tpg262->GetPressures( p1, p2)) {
+
+    if (argc==1) {
+      std::cout.precision(2);
+      std::cout.setf(std::ios::scientific, std::ios::floatfield);
+      std::ofstream ofile("ReadTPG262.last");
+      ofile.precision(2);
+      ofile.setf(std::ios::scientific, std::ios::floatfield);
+      ofile << p1.first << "  " << p1.second << "  ";
+      ofile << p2.first << "  " << p2.second << std::endl;
+    } else {
+      std::ofstream ofile(argv[1], std::fstream::app);
+      if (ofile.good()) {
+	time_t seconds = time(NULL);
+	ofile << seconds << "   ";
+	ofile.precision(2);
+	ofile.setf(std::ios::scientific, std::ios::floatfield);
+	ofile << p1.first << "  " << p1.second << "  ";
+	ofile << p2.first << "  " << p2.second << std::endl;
+      }
     }
-  }
 
+    sleep(10);
+
+  }
+  
   delete tpg262;
   
   return 0;
