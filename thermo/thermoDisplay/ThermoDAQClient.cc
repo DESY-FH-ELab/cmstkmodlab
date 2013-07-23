ef9a931b670868cf87e3e16aaed21384d369db00
Negusbuk <andreas@mussgiller.de>
2013-07-23 08:03:52 +0000

    added timeout when trying to connect to thermoDAQ

--- thermo/thermoDisplay/ThermoDAQClient.cc
+++ thermo/thermoDisplay/ThermoDAQClient.cc
@@ -17,16 +17,21 @@ void ThermoDAQClient::readDAQStatus()
     std::cout << "ThermoDAQClient::readDAQStatus()" << std::endl;
 
     blockSize_ = 0;
-    tcpSocket_->abort();
     tcpSocket_->connectToHost(QHostAddress::LocalHost, socket_);
+    if (!tcpSocket_->waitForConnected(2000)) {
+        tcpSocket_->abort();
+        std::cout << "connection timeout" << std::endl;
+    }
 }
 
 void ThermoDAQClient::read()
 {
-    std::cout << "ThermoDAQClient::read()" << std::endl;
+    // std::cout << "ThermoDAQClient::read()" << std::endl;
 
     QByteArray in = tcpSocket_->readAll();
     QString buffer(in.data());
+
+    tcpSocket_->abort();
 
     emit handleMessage(buffer);
 }
