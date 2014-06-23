387cd1c938a139530e7f1b71b035aff4a51cf2eb
DESY-FH-ELab <fhluser@fhltkmodlab1.desy.de>
2014-06-23 16:36:42 +0200

    new version of test code

--- devices/Iota/test.cc
+++ devices/Iota/test.cc
@@ -15,25 +15,27 @@ int main()
 {
   std::cout << "test" << std::endl;
   
-  Iota300_t iota("/dev/ttyS4");
+  Iota300_t iota("/dev/ttyS3");
   
-  iota.SetFlow(7);
+//   iota.SetFlow(7);
+  std::cout << "flow = " << iota.GetSetFlow() << std::endl;
+  std::cout << "pressure = " << iota.GetSetPressure() << std::endl;
 
-  long suffering = 0;
-
-  while (1) {
-    std::cout << "flow = " << iota.GetFlow() << std::endl;
-    float pressure = iota.GetPressure();
-    std::cout << "pressure = " << pressure << std::endl;
-    std::cout << "status = " << (int)iota.GetStatus() << std::endl;
-    if (fabs(pressure-12.34)>0.01) break;
-    std::cout << std::endl;
-    usleep(500000);
-
-    suffering++;
-  
-    std::cout << "suffering: " << suffering << std::endl;
-  }
+//   long suffering = 0;
+// 
+//   while (1) {
+//     std::cout << "flow = " << iota.GetFlow() << std::endl;
+//     float pressure = iota.GetPressure();
+//     std::cout << "pressure = " << pressure << std::endl;
+//     std::cout << "status = " << (int)iota.GetStatus() << std::endl;
+//     if (fabs(pressure-12.34)>0.01) break;
+//     std::cout << std::endl;
+//     usleep(500000);
+// 
+//     suffering++;
+//   
+//     std::cout << "suffering: " << suffering << std::endl;
+//   }
 
   return 0;
 }
