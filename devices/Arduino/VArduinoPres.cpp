b642d69a352678032e7cb0cc2d0d19750157cb47
Negusbuk <Andreas Mussgiller>
2014-06-25 23:47:33 +0200

    added calibration function to convert from ADC counts to pressure

--- devices/Arduino/VArduinoPres.cpp
+++ devices/Arduino/VArduinoPres.cpp
@@ -7,3 +7,12 @@ VArduinoPres::VArduinoPres( const ioport_t ioPort )
 {
 
 }
+
+float VArduinoPres::ToPressure(int value) const
+{
+  // 5V = 1023 = 100 bar
+
+  if (value>1023) return 100.;
+  float ret = value;
+  return 100.*ret/1023.;
+}
