4e74473f8fe5f445854964932acc9cfa3c0d26d1
Negusbuk <andreas@mussgiller.de>
2015-03-19 09:02:49 +0100

    make use of new widget for light panel control

--- defo/defoDAQ/DefoConradWidget.cc
+++ defo/defoDAQ/DefoConradWidget.cc
@@ -1,10 +1,8 @@
 #include "DefoConradWidget.h"
 #include <QCheckBox>
 #include <QVBoxLayout>
-#include <QGridLayout>
 
 #include  <sstream>
-
 
 DefoConradWidget::DefoConradWidget(DefoConradModel* model,
 				   QWidget *parent)
@@ -14,52 +12,8 @@ DefoConradWidget::DefoConradWidget(DefoConradModel* model,
 
   layout->addWidget(new DefoConradDeviceCheckbox(model, "Enable switch", this));
 
-  QGridLayout* glayout = new QGridLayout();
-  glayout->setSpacing(1);
-
-  DefoConradSwitchButton* button;
-
-  glayout->addWidget(button = new DefoConradSwitchButton(model,
-							 static_cast<DefoConradModel::DeviceSwitch>(2),
-							 QString("Panel 3"),
-							 this),
-		     0, 0, 2, 1);
-  button->setMinimumWidth(100);
-  button->setMinimumHeight(200);
-
-  glayout->addWidget(button = new DefoConradSwitchButton(model,
-							 static_cast<DefoConradModel::DeviceSwitch>(0),
-							 QString("Panel 1"),
-							 this),
-		     0, 1, 1, 1);
-  button->setMinimumWidth(100);
-  button->setMinimumHeight(100);
-
-  glayout->addWidget(button = new DefoConradSwitchButton(model,
-							 static_cast<DefoConradModel::DeviceSwitch>(1),
-							 QString("Panel 2"),
-							 this),
-		     1, 1, 1, 1);
-  button->setMinimumWidth(100);
-  button->setMinimumHeight(100);
-
-  glayout->addWidget(button = new DefoConradSwitchButton(model,
-							 static_cast<DefoConradModel::DeviceSwitch>(3),
-							 QString("Panel 4"),
-							 this),
-		     0, 2, 2, 1);
-  button->setMinimumWidth(100);
-  button->setMinimumHeight(200);
-
-  glayout->addWidget(button = new DefoConradSwitchButton(model,
-							 static_cast<DefoConradModel::DeviceSwitch>(4),
-							 QString("Panel 5"),
-							 this),
-		     2, 0, 1, 3);
-  button->setMinimumWidth(300);
-  button->setMinimumHeight(100);
-
-  layout->addLayout(glayout);
+  DefoConradPanelWidget* panels = new DefoConradPanelWidget(model, this);
+  layout->addWidget(panels);
 
   for ( int i = 0; i < 5; ++i)
     layout->addWidget(new DefoConradSwitchCheckbox(model,
