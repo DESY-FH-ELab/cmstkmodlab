b267cdd6e6dc1445801331fc314a0993925b7286
Negusbuk <Andreas Mussgiller>
2015-04-01 16:26:08 +0200

    added a new subroutine with arrays

--- external/ddierckx/testF.f
+++ external/ddierckx/testF.f
@@ -1,4 +1,4 @@
-      subroutine fortfunc(ii,ff)
+      subroutine fortfunc1(ii,ff)
       integer ii
       real*4  ff
 
@@ -7,3 +7,14 @@
 
       return
       end
+
+      subroutine fortfunc2(ii,ff)
+      integer ii
+      real*4  ff(ii)
+
+      write(6,200) ii, ff(5)
+ 200  format('ii=',i2,' ff=',f6.3)
+
+      return
+      end
+      
