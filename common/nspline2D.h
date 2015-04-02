9a8edbd7bfaf97fea3b7d60cedb1ef803277a3b6
Negusbuk <Andreas Mussgiller>
2015-04-02 09:05:01 +0200

    first working version of 2D spline fit based on ddierckx

--- common/nspline2D.h
+++ common/nspline2D.h
@@ -3,8 +3,9 @@
 #define NSPLINE2D_H
 
 #include <vector>
-#include <utility>
-#include <cmath>
+
+#include <npoint2D.h>
+#include <npoint3D.h>
 
 class NSpline2D
 {
@@ -13,6 +14,40 @@ class NSpline2D
 
   NSpline2D();
 
+  void surfit(const std::vector<double>& x,
+              const std::vector<double>& y,
+              const std::vector<double>& z,
+              const std::vector<double>& w,
+              int kx = 3, int ky = 3, double s = 0);
+
+  void surfit(const std::vector<double>& x,
+              const std::vector<double>& y,
+              const std::vector<double>& z,
+              int kx = 3, int ky = 3, double s = 0);
+
+  void regrid(const std::vector<double>& x,
+              const std::vector<double>& y,
+              const std::vector<double>& z,
+              int kx = 3, int ky = 3, double s = 0);
+
+  bool evaluate(double x, double y, double& z);
+
+  bool evaluate(const std::vector<double>& x,
+                const std::vector<double>& y,
+                std::vector<double>& z);
+
+ protected:
+
+  int calcSurfitLwrk1(int m,
+                      int ky, int kx,
+                      int nyest, int nxest);
+
+  std::vector<double> tx_;
+  std::vector<double> ty_;
+  std::vector<double> c_;
+  int kx_;
+  int ky_;
+  double fp_;
 };
 
 #endif //NSPLINE2D_H
