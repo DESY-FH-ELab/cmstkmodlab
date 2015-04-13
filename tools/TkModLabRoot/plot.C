3d5888ea81dc15b5296f4f067a69b992c2bbdb1f
Negusbuk <andreas@mussgiller.de>
2015-04-13 15:00:48 +0200

    update of test code and more data files added

--- tools/TkModLabRoot/plot.C
+++ tools/TkModLabRoot/plot.C
@@ -1,31 +1,37 @@
 void plot()
 {
   TCanvas * c = new TCanvas("c", "c", 700, 500);
-
+  
   int ix, iy;
   double x, y, zx, zy;
   double hx, hy;
-
+  
   TGraph2D *gr1 = new TGraph2D();
   int nPoints1 = 0;
-
-  std::ifstream ifile1("defoReco_20150401074552_20150401074653.txt");
+  
+  //std::ifstream ifile1("defoReco_20150330095913_20150330100348.txt");
+  //std::ifstream ifile1("defoReco_20150401074552_20150401074653.txt");
+  //std::ifstream ifile1("defoReco_20150401074552_20150401074812.txt");
+  std::ifstream ifile1("defoReco_20150401143932_20150401144027.txt");
   while (ifile1 >> ix >> iy >> x >> y >> zx >> hx >> zy >> hy) {
     zx *= 1e3;
     zy *= 1e3;
-
+    
     gr1->SetPoint(nPoints1, x, y, zx);
     nPoints1++;
   }
-
-  gr1->Draw("SURF1Z");
-
-  c->Print("defoReco.png");
-
+  
+  // gr1->Draw("SURF1Z");
+  // gr1->Draw("P0");
+  
+  // c->Print("defoReco.png");
+  
   TkModLabSpline2D spline;
 
-  spline.Surfit(gr1->GetN(),
-                gr1->GetX(), gr1->GetY(), gr1->GetZ());
+  std::cout << spline.Surfit(gr1->GetN(),
+			     gr1->GetX(), gr1->GetY(), gr1->GetZ(),
+			     3, 3, 1,
+			     0.75) << std::endl;
 
   TGraph *gr21D = new TGraph();
   TGraph2D *gr2 = new TGraph2D();
@@ -37,9 +43,12 @@ void plot()
     gr2->SetPoint(nPoints2++, gr1->GetX()[i], gr1->GetY()[i], z);
   }
 
-  gr2->Draw("SURF1Z");
+  // gr2->Draw("SURF1Z");
+  // gr2->Draw("P0");
 
-  c->Print("defoRecoSpline2D.png");
+  // c->Print("defoRecoSpline2D.png");
+
+  // return;
 
   TGraph2D *gr3 = new TGraph2D();
   int nPoints3 = 0;
@@ -49,8 +58,8 @@ void plot()
     spline.Evaluate(gr1->GetX()[i], gr1->GetY()[i], z);
     gr3->SetPoint(nPoints3++, gr1->GetX()[i], gr1->GetY()[i], z - gr1->GetZ()[i]);
   }
-
+  
   gr3->Draw("SURF1Z");
 
-  c->Print("defoRecoSpline2DMinusReco.png");
+  // c->Print("defoRecoSpline2DMinusReco.png");
 }
