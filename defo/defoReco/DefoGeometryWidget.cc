3339c1c68a42e1e6fb5195c73737a4ef6d40c2c7
Negusbuk <andreas@mussgiller.de>
2015-04-14 09:58:09 +0200

    split geometry parameter widget into a display and an edit part

--- defo/defoReco/DefoGeometryWidget.cc
+++ defo/defoReco/DefoGeometryWidget.cc
@@ -56,81 +56,6 @@ DefoGeometryWidget::DefoGeometryWidget(DefoGeometryModel* geometryModel,
   sketch_ = new DefoSvgWidget(this);
   layout->addWidget(sketch_);
 
-  QGridLayout *grid = new QGridLayout();
-  grid->setContentsMargins(0, 0, 0, 0);
-  QWidget * inputs = new QWidget(this);
-  inputs->setLayout(grid);
-  layout->addWidget(inputs);
-
-  angle1SpinBox_ = new QDoubleSpinBox(inputs);
-  angle1SpinBox_->setPrefix("a1 = ");
-  angle1SpinBox_->setSuffix(" deg");
-  angle1SpinBox_->setRange(10.0, 45.0);
-  angle1SpinBox_->setDecimals(2);
-  angle1SpinBox_->setSingleStep(0.01);
-  grid->addWidget(angle1SpinBox_, 0, 0);
-  angle1SpinBox_->setValue(geometryModel_->getAngle1());
-  connect(angle1SpinBox_, SIGNAL(valueChanged(double)),
-          this, SLOT(angle1Changed(double)));
-
-  angle2SpinBox_ = new QDoubleSpinBox(inputs);
-  angle2SpinBox_->setPrefix("a2 = ");
-  angle2SpinBox_->setSuffix(" deg");
-  angle2SpinBox_->setRange(10.0, 45.0);
-  angle2SpinBox_->setDecimals(2);
-  angle2SpinBox_->setSingleStep(0.01);
-  grid->addWidget(angle2SpinBox_, 0, 1);
-  angle2SpinBox_->setValue(geometryModel_->getAngle2());
-  connect(angle2SpinBox_, SIGNAL(valueChanged(double)),
-          this, SLOT(angle2Changed(double)));
-
-  angle3SpinBox_ = new QDoubleSpinBox(inputs);
-  angle3SpinBox_->setPrefix("a3 = ");
-  angle3SpinBox_->setSuffix(" deg");
-  angle3SpinBox_->setRange(-10.0, 10.0);
-  angle3SpinBox_->setDecimals(2);
-  angle3SpinBox_->setSingleStep(0.01);
-  grid->addWidget(angle3SpinBox_, 0, 2);
-  angle3SpinBox_->setValue(geometryModel_->getAngle3());
-  connect(angle3SpinBox_, SIGNAL(valueChanged(double)),
-          this, SLOT(angle3Changed(double)));
-
-  distanceSpinBox_ = new QDoubleSpinBox(inputs);
-  distanceSpinBox_->setPrefix("d = ");
-  distanceSpinBox_->setSuffix(" mm");
-  distanceSpinBox_->setRange(100.0, 999.0);
-  distanceSpinBox_->setDecimals(0);
-  distanceSpinBox_->setSingleStep(1.0);
-  grid->addWidget(distanceSpinBox_, 0, 3);
-  distanceSpinBox_->setValue(geometryModel_->getDistance());
-  connect(distanceSpinBox_, SIGNAL(valueChanged(double)),
-          this, SLOT(distanceChanged(double)));
-
-  height1SpinBox_ = new QDoubleSpinBox(inputs);
-  height1SpinBox_->setPrefix("h1 = ");
-  height1SpinBox_->setSuffix(" mm");
-  height1SpinBox_->setRange(1000.0, 2000.0);
-  height1SpinBox_->setDecimals(0);
-  height1SpinBox_->setSingleStep(1.0);
-  grid->addWidget(height1SpinBox_, 0, 4);
-  height1SpinBox_->setValue(geometryModel_->getHeight1());
-  connect(height1SpinBox_, SIGNAL(valueChanged(double)),
-          this, SLOT(height1Changed(double)));
-
-  height2SpinBox_ = new QDoubleSpinBox(inputs);
-  height2SpinBox_->setPrefix("h2 = ");
-  height2SpinBox_->setSuffix(" mm");
-  height2SpinBox_->setRange(0.0, 500.0);
-  height2SpinBox_->setDecimals(0);
-  height2SpinBox_->setSingleStep(1.0);
-  grid->addWidget(height2SpinBox_, 0, 5);
-  height2SpinBox_->setValue(geometryModel_->getHeight2());
-  connect(height2SpinBox_, SIGNAL(valueChanged(double)),
-          this, SLOT(height2Changed(double)));
-
-  //grid->addWidget(new QWidget(inputs), 1, 3);
-  //grid->addWidget(new QWidget(inputs), 1, 4);
-
   connect(geometryModel_,
           SIGNAL(geometryChanged()),
           this,
@@ -172,41 +97,7 @@ void DefoGeometryWidget::updateSketch() {
   sketch_->load(tempSketch.toLocal8Bit());
 }
 
-void DefoGeometryWidget::angle1Changed(double v) {
-  geometryModel_->setAngle1(v);
+void DefoGeometryWidget::geometryChanged()
+{
   updateSketch();
-}
-
-void DefoGeometryWidget::angle2Changed(double v) {
-  geometryModel_->setAngle2(v);
-  updateSketch();
-}
-
-void DefoGeometryWidget::angle3Changed(double v) {
-  geometryModel_->setAngle3(v);
-  updateSketch();
-}
-
-void DefoGeometryWidget::distanceChanged(double v) {
-  geometryModel_->setDistance(v);
-  updateSketch();
-}
-
-void DefoGeometryWidget::height1Changed(double v) {
-  geometryModel_->setHeight1(v);
-  updateSketch();
-}
-
-void DefoGeometryWidget::height2Changed(double v) {
-  geometryModel_->setHeight2(v);
-  updateSketch();
-}
-
-void DefoGeometryWidget::geometryChanged() {
-  angle1SpinBox_->setValue(geometryModel_->getAngle1());
-  angle2SpinBox_->setValue(geometryModel_->getAngle2());
-  angle3SpinBox_->setValue(geometryModel_->getAngle3());
-  distanceSpinBox_->setValue(geometryModel_->getDistance());
-  height1SpinBox_->setValue(geometryModel_->getHeight1());
-  height2SpinBox_->setValue(geometryModel_->getHeight2());
 }
--- defo/defoReco/DefoGeometryWidget.h
+++ defo/defoReco/DefoGeometryWidget.h
@@ -34,21 +34,8 @@ protected:
   DefoSvgWidget * sketch_ ;
   void updateSketch();
 
-  QDoubleSpinBox * angle1SpinBox_;
-  QDoubleSpinBox * angle2SpinBox_;
-  QDoubleSpinBox * angle3SpinBox_;
-  QDoubleSpinBox * distanceSpinBox_;
-  QDoubleSpinBox * height1SpinBox_;
-  QDoubleSpinBox * height2SpinBox_;
-
 protected slots:
 
-  void angle1Changed(double);
-  void angle2Changed(double);
-  void angle3Changed(double);
-  void distanceChanged(double);
-  void height1Changed(double);
-  void height2Changed(double);
   void geometryChanged();
 };
 
