aa82d293660a7af07be693b3f07c02c842c11175
nilsflaschel <nils.flaschel@desy.de>
2015-04-16 16:59:16 +0200

    test changed to flow

--- devices/Cori/test.cc
+++ devices/Cori/test.cc
@@ -17,13 +17,13 @@ int main()
   CoriFlow_t cori("/dev/ttyS2");
   
 //   std::cout << "Temperature = " << cori.getTemp() << std::endl;
-//   std::cout << "Flow = " << cori.getMeasure() << std::endl;
+  std::cout << "Flow = " << cori.getMeasure() << std::endl;
 //   std::cout << "Pressure = " << cori.getPres() << std::endl;
 //   std::cout << "Capacity 100% = " << cori.getCapacity() << std::endl;
 //   std::cout << "Density = " << cori.getDensity() << std::endl;
 //   std::cout << "Unit = " << cori.getUnit() << std::endl;
 //   std::cout << "Fluid = " << cori.getFluid() << std::endl;
-  std::cout << "User Tag = " << cori.getTag() << std::endl;
+//   std::cout << "User Tag = " << cori.getTag() << std::endl;
 //   std::cout << "Init status = " << cori.getInit() << std::endl;
 //   std::cout << "set Capacity 100% = " << cori.setCapacity() << std::endl;
 //   std::cout << "Capacity 100% = " << cori.getCapacity() << std::endl;
