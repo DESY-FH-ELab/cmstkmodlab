45a2ca33c351924a83315587c1c1af3516602706
Negusbuk <Andreas Mussgiller>
2015-09-10 11:30:18 +0200

    made all parameters GUI configurable

--- assembly/assemblyCommon/AssemblySensorMarkerFinderWidget.cc
+++ assembly/assemblyCommon/AssemblySensorMarkerFinderWidget.cc
@@ -1,8 +1,10 @@
 #include <QFormLayout>
 #include <QSpinBox>
 #include <QDoubleSpinBox>
+#include <QCheckBox>
 
 #include <nqlogger.h>
+#include <IntegerComboBox.h>
 
 #include "AssemblySensorMarkerFinderWidget.h"
 
@@ -14,28 +16,36 @@ AssemblySensorMarkerFinderWidget::AssemblySensorMarkerFinderWidget(AssemblySenso
     QFormLayout* layout;
     QSpinBox* ispin;
     QDoubleSpinBox* dspin;
+    QCheckBox* checkbox;
+    IntegerComboBox* icb;
     QWidget* w;
 
     w = new QWidget(this);
     layout = new QFormLayout(w);
 
-    ispin = new QSpinBox(w);
-    ispin->setMinimum(1);
-    ispin->setMaximum(21);
-    ispin->setSingleStep(2);
-    ispin->setValue(finder_->gaussianBlurKernelSize());
-    connect(ispin, SIGNAL(valueChanged(int)),
+    icb = new IntegerComboBox(w);
+    icb->addItem(0);
+    icb->addItem(1);
+    icb->addItem(3);
+    icb->addItem(5);
+    icb->addItem(7);
+    icb->addItem(9);
+    icb->addItem(11);
+    icb->addItem(13);
+    icb->addItem(15);
+    icb->setCurrentIndex(icb->findData(QVariant(finder_->gaussianBlurKernelSize())));
+    connect(icb, SIGNAL(valueChanged(int)),
             finder_, SLOT(setGaussianBlurKernelSize(int)));
-    layout->addRow("Kernel Size:", ispin);
+    layout->addRow("Kernel Size [pixel]:", icb);
 
     ispin = new QSpinBox(w);
-    ispin->setMinimum(1);
+    ispin->setMinimum(0);
     ispin->setMaximum(9);
-    ispin->setSingleStep(2);
+    ispin->setSingleStep(1);
     ispin->setValue(finder_->gaussianBlurSigma());
     connect(ispin, SIGNAL(valueChanged(int)),
             finder_, SLOT(setGaussianBlurSigma(int)));
-    layout->addRow("Sigma:", ispin);
+    layout->addRow("Sigma [pixel]:", ispin);
 
     w->setLayout(layout);
     addItem(w, "Gaussian Blur");
@@ -68,8 +78,93 @@ AssemblySensorMarkerFinderWidget::AssemblySensorMarkerFinderWidget(AssemblySenso
     dspin->setValue(finder_->expectedCircleRadius());
     connect(dspin, SIGNAL(valueChanged(double)),
             finder_, SLOT(setExpectedCircleSize(double)));
-    layout->addRow("Expected Circle Radius:", dspin);
+    layout->addRow("Expected Circle Radius [pixel]:", dspin);
 
     w->setLayout(layout);
     addItem(w, "Circle Detection");
+
+    w = new QWidget(this);
+    layout = new QFormLayout(w);
+
+    ispin = new QSpinBox(w);
+    ispin->setMinimum(5);
+    ispin->setMaximum(255);
+    ispin->setSingleStep(1);
+    ispin->setValue(finder_->linesCannyEdgeDetectionThreshold1());
+    connect(ispin, SIGNAL(valueChanged(int)),
+            finder_, SLOT(setLinesCannyEdgeDetectionThreshold1(int)));
+    layout->addRow("Canny Threshold 1:", ispin);
+
+    ispin = new QSpinBox(w);
+    ispin->setMinimum(5);
+    ispin->setMaximum(255);
+    ispin->setSingleStep(1);
+    ispin->setValue(finder_->linesCannyEdgeDetectionThreshold2());
+    connect(ispin, SIGNAL(valueChanged(int)),
+            finder_, SLOT(setLinesCannyEdgeDetectionThreshold2(int)));
+    layout->addRow("Canny Threshold 2:", ispin);
+
+    icb = new IntegerComboBox(w);
+    icb->addItem(1);
+    icb->addItem(3);
+    icb->addItem(5);
+    icb->addItem(7);
+    icb->setCurrentIndex(icb->findData(QVariant(finder_->linesCannyEdgeDetectionApertureSize())));
+    connect(icb, SIGNAL(valueChanged(int)),
+            finder_, SLOT(setLinesCannyEdgeDetectionApertureSize(int)));
+    layout->addRow("Canny Aperture Size:", icb);
+
+    checkbox = new QCheckBox(w);
+    checkbox->setChecked(finder_->linesCannyEdgeDetectionL2Gradient());
+    connect(checkbox, SIGNAL(clicked(bool)),
+            finder_, SLOT(setLinesCannyEdgeDetectionL2Gradient(bool)));
+    layout->addRow("Canny L2 Gradient:", checkbox);
+
+    dspin = new QDoubleSpinBox(w);
+    dspin->setMinimum(1);
+    dspin->setMaximum(100);
+    dspin->setSingleStep(0.1);
+    dspin->setValue(finder_->linesHoughDistanceResolution());
+    connect(dspin, SIGNAL(valueChanged(double)),
+            finder_, SLOT(setLinesHoughDistanceResolution(double)));
+    layout->addRow("Hough Line Detection Distance Resolution [pixel]:", dspin);
+
+    dspin = new QDoubleSpinBox(w);
+    dspin->setMinimum(0.1);
+    dspin->setMaximum(10.0);
+    dspin->setSingleStep(0.1);
+    dspin->setValue(finder_->linesHoughAngleResolution());
+    connect(dspin, SIGNAL(valueChanged(double)),
+            finder_, SLOT(setLinesHoughAngleResolution(double)));
+    layout->addRow("Hough Line Detection Angle Resolution [deg]:", dspin);
+
+    ispin = new QSpinBox(w);
+    ispin->setMinimum(5);
+    ispin->setMaximum(255);
+    ispin->setSingleStep(1);
+    ispin->setValue(finder_->linesHoughThreshold());
+    connect(ispin, SIGNAL(valueChanged(int)),
+            finder_, SLOT(setLinesHoughThreshold(int)));
+    layout->addRow("Hough Line Detection Threshold:", ispin);
+
+    dspin = new QDoubleSpinBox(w);
+    dspin->setMinimum(10);
+    dspin->setMaximum(200);
+    dspin->setSingleStep(0.1);
+    dspin->setValue(finder_->linesHoughMinLineLength());
+    connect(dspin, SIGNAL(valueChanged(double)),
+            finder_, SLOT(setLinesHoughMinLineLength(double)));
+    layout->addRow("Hough Line Detection Minimum Line Length:", dspin);
+
+    dspin = new QDoubleSpinBox(w);
+    dspin->setMinimum(10);
+    dspin->setMaximum(200);
+    dspin->setSingleStep(0.1);
+    dspin->setValue(finder_->linesHoughMaxLineGap());
+    connect(dspin, SIGNAL(valueChanged(double)),
+            finder_, SLOT(setLinesHoughHoughMaxLineGap(double)));
+    layout->addRow("Hough Line Detection Maximum Line Gap:", dspin);
+
+    w->setLayout(layout);
+    addItem(w, "Line Detection");
 }
