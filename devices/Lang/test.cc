f376bab28e18712a9680e38420be056696de902c
Negusbuk <andreas@mussgiller.de>
2015-10-22 12:50:53 +0200

    added more test

--- devices/Lang/test.cc
+++ devices/Lang/test.cc
@@ -1,3 +1,5 @@
+#include <string>
+#include <vector>
 #include <iostream>
 
 #ifdef USE_FAKEIO
@@ -8,13 +10,51 @@ typedef LStepExpressFake LStepExpress_t;
 typedef LStepExpress LStepExpress_t;
 #endif
 
+template<typename T, typename A> void dumpVector(const std::vector<T,A>& v)
+{
+  for (typename std::vector<T,A>::const_iterator it = v.begin();
+       it!=v.end();
+       ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+}
+
 int main()
 {
   LStepExpress_t lstep("/dev/ttyUSB0");
 
-  lstep.SendCommand("!reset");
-  lstep.SetValue("!axisdir", VLStepExpress::X, 1);
-  lstep.SetValue("!axisdir", VLStepExpress::A, 0);
+  //lstep.SendCommand("!reset");
+  //lstep.SetValue("!axisdir", VLStepExpress::X, 1);
+  //lstep.SetValue("!axisdir", VLStepExpress::A, 0);
+
+  std::string svalue;
+  int ivalue;
+  double dvalue;
+  std::vector<int> ivalues;
+  std::vector<double> dvalues;
+
+  lstep.GetDimension(ivalues);
+  dumpVector(ivalues);
+  lstep.GetDimension(VLStepExpress::Z, ivalue);
+  std::cout << ivalue << std::endl;
+  
+  ivalues[VLStepExpress::Y] = 1;
+  lstep.SetDimension(ivalues);
+  lstep.GetDimension(ivalues);
+  dumpVector(ivalues);
+  
+  lstep.SetDimension(VLStepExpress::Y, 2);
+  lstep.GetDimension(ivalues);
+  dumpVector(ivalues);
+
+  lstep.GetPowerAmplifierStatus(ivalues);
+  dumpVector(ivalues);
+  lstep.GetPowerAmplifierStatus(VLStepExpress::Y, ivalue);
+  std::cout << ivalue << std::endl;
+
+  lstep.GetPosition(dvalues);
+  dumpVector(dvalues);
 
   return 0;
 }
