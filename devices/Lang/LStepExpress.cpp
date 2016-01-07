81bbd4b20f88c2a1b5cf1b0e82a840d85ae30fc6
Negusbuk <andreas@mussgiller.de>
2016-01-07 10:53:52 +0100

    update of device serial number

--- devices/Lang/LStepExpress.cpp
+++ devices/Lang/LStepExpress.cpp
@@ -84,7 +84,7 @@ void LStepExpress::DeviceInit()
     StripBuffer(buffer);
     unsigned long serialNumber = std::atol(buffer);
 
-    if (serialNumber != 40051635759) {
+    if (serialNumber != 40052435759) {
       isDeviceAvailable_ = false;
       return;
     }
