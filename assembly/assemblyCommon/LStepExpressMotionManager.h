e71ed73c47bfb71f5c71b50594bbbcd824450b2f
Andreas Mussgiller <andreas@mussgiller.de>
2016-01-12 11:10:31 +0100

    moved motion storage class to dedicated files

--- assembly/assemblyCommon/LStepExpressMotionManager.h
+++ assembly/assemblyCommon/LStepExpressMotionManager.h
@@ -4,26 +4,7 @@
 #include <QQueue>
 
 #include "LStepExpressModel.h"
-
-class LStepExpressMotion
-{
-public:
-
-    LStepExpressMotion(std::vector<double> & values, bool mode);
-    LStepExpressMotion(double x, double y, double z, double a, bool mode);
-    LStepExpressMotion(unsigned int axis, double value, bool mode);
-
-    bool getMode() const { return mode_; }
-    double getX() const { return x_; }
-    double getY() const { return y_; }
-    double getZ() const { return z_; }
-    double getA() const { return a_; }
-
-protected:
-
-    bool mode_;
-    double x_, y_, z_, a_;
-};
+#include "LStepExpressMotion.h"
 
 class LStepExpressMotionManager : public QObject
 {
