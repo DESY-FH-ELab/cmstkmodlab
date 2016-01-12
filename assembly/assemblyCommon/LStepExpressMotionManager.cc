a95beec45a455c2b4b216ba645af6246c3c24306
Negusbuk <andreas@mussgiller.de>
2016-01-12 14:49:34 +0100

    remove two second sleep

--- assembly/assemblyCommon/LStepExpressMotionManager.cc
+++ assembly/assemblyCommon/LStepExpressMotionManager.cc
@@ -39,8 +39,6 @@ void LStepExpressMotionManager::run()
     } else {
         emit signalMoveRelative(motion.getX(), motion.getY(), motion.getZ(), motion.getA());
     }
-
-    sleep(2);
 }
 
 void LStepExpressMotionManager::myMoveToThread(QThread *thread)
