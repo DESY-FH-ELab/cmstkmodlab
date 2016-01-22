96a9b3f238d87a45f4ce29acaff3185d5cf86686
Negusbuk <andreas@mussgiller.de>
2016-01-22 10:48:35 +0100

    fixed check for device serial number

--- devices/Lang/LStepExpress.cpp
+++ devices/Lang/LStepExpress.cpp
@@ -93,7 +93,7 @@ void LStepExpress::DeviceInit()
     StripBuffer(buffer);
     unsigned long serialNumber = std::atol(buffer);
 
-    if (serialNumber != 40052435759 || serialNumber != 40051635759) {
+    if (!(serialNumber==40052435759 || serialNumber==40051635759)) {
       isDeviceAvailable_ = false;
       return;
     }
