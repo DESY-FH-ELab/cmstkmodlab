f436ee4286c79df68ce96b34a594d4a954ce0c73
Andreas Mussgiller <andreas@mussgiller.de>
2016-09-19 15:57:01 +0200

    renamed polarity bits enum to something more generic

--- devices/Nanotec/VNanotecSMCI36.h
+++ devices/Nanotec/VNanotecSMCI36.h
@@ -72,7 +72,7 @@ class VNanotecSMCI36
     smciOInvalid                 = 4
   };
 
-  enum ReversePolaryBits {
+  enum IOBits {
     smciInput1            = 0x00000001,
     smciInput2            = 0x00000002,
     smciInput3            = 0x00000004,
