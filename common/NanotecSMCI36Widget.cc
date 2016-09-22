688aa65b3d2a6a295c443353b6f6378ef07b6fa5
Andreas Mussgiller <andreas@mussgiller.de>
2016-09-22 14:54:30 +0200

    added more features

--- common/NanotecSMCI36Widget.cc
+++ common/NanotecSMCI36Widget.cc
@@ -1,10 +1,57 @@
 #include <iostream>
 
 #include <QGridLayout>
+#include <QFormLayout>
 
 #include <nqlogger.h>
 
 #include "NanotecSMCI36Widget.h"
+
+NanotecSMCI36PositioningModeWidget::NanotecSMCI36PositioningModeWidget(NanotecSMCI36Model* model,
+                                                                       QWidget *parent)
+  : QComboBox(parent),
+    model_(model)
+{
+  // No user editable text
+  setEditable(false);
+
+  const std::vector<std::pair<int,std::string>>& modes = model_->getPositioningModeNames();
+
+  for (std::vector<std::pair<int,std::string>>::const_iterator it = modes.begin();
+       it!=modes.end();
+       ++it) {
+    addItem(it->second.c_str(), QVariant(it->first));
+  }
+
+  connect(model_, SIGNAL(informationChanged()),
+          this, SLOT(updateInfo()));
+
+  connect(this, SIGNAL(currentIndexChanged(int)),
+          this, SLOT(currentChanged(int)));
+}
+
+void NanotecSMCI36PositioningModeWidget::indexChanged(int index)
+{
+  // NQLog("NanotecSMCI36PositioningModeWidget", NQLog::Debug) << "indexChanged()";
+
+  unsigned int userValue = itemData(index).toUInt();
+
+  if (model_->getPositioningMode()!=userValue) {
+    model_->setPositioningMode(userValue);
+  }
+}
+
+void NanotecSMCI36PositioningModeWidget::updateInfo()
+{
+  // NQLog("NanotecSMCI36PositioningModeWidget", NQLog::Debug) << "updateInfo()";
+
+  unsigned int mode = model_->getPositioningMode();
+  int index = findData(mode);
+  if (index!=currentIndex()) {
+    setCurrentIndex(index);
+  }
+}
+
 
 NanotecSMCI36Widget::NanotecSMCI36Widget(NanotecSMCI36Model* model, QWidget *parent)
   : QWidget(parent),
@@ -16,6 +63,52 @@ NanotecSMCI36Widget::NanotecSMCI36Widget(NanotecSMCI36Model* model, QWidget *par
   smci36CheckBox_ = new QCheckBox("Enable controller", this);
   layout->addWidget(smci36CheckBox_);
 
+  QFormLayout *formLayout = new QFormLayout;
+
+  status_ = new QLabel("         ", this);
+  formLayout->addRow("status", status_);
+
+  motorID_ = new QSpinBox(this);
+  formLayout->addRow("motor ID", motorID_);
+
+  controllerSteps_ = new QLabel("         ", this);
+  formLayout->addRow("controller steps", controllerSteps_);
+
+  encoderSteps_ = new QLabel("         ", this);
+  formLayout->addRow("encoder steps", encoderSteps_);
+
+  positioningMode_ = new NanotecSMCI36PositioningModeWidget(model_, this);
+  formLayout->addRow("positioning mode", positioningMode_);
+
+  direction_ = new QCheckBox(this);
+  connect(direction_, SIGNAL(clicked(bool)),
+          model_, SLOT(setDirection(bool)));
+  formLayout->addRow("direction", direction_);
+
+  travelDistance_ = new QDoubleSpinBox(this);
+  travelDistance_->setMinimum(-100000);
+  travelDistance_->setMaximum(+100000);
+  connect(travelDistance_, SIGNAL(valueChanged(double)),
+          model_, SLOT(setTravelDistance(double)));
+  formLayout->addRow("travel distance", travelDistance_);
+
+  layout->addLayout(formLayout);
+
+  start_ = new QPushButton("start", this);
+  connect(start_, SIGNAL(clicked()),
+          model_, SLOT(start()));
+  layout->addWidget(start_);
+
+  stop_ = new QPushButton("stop", this);
+  connect(stop_, SIGNAL(clicked()),
+          model_, SLOT(stop()));
+  layout->addWidget(stop_);
+
+  quickstop_ = new QPushButton("quick stop", this);
+  connect(quickstop_, SIGNAL(clicked()),
+          model_, SLOT(stop()));
+  layout->addWidget(quickstop_);
+
   // Connect all the signals
   connect(model_, SIGNAL(deviceStateChanged(State)),
           this, SLOT(updateDeviceState(State)));
@@ -26,6 +119,9 @@ NanotecSMCI36Widget::NanotecSMCI36Widget(NanotecSMCI36Model* model, QWidget *par
 
   connect(smci36CheckBox_, SIGNAL(toggled(bool)),
           model_, SLOT(setDeviceEnabled(bool)));
+
+  connect(motorID_, SIGNAL(valueChanged(int)),
+          model_, SLOT(setMotorID(int)));
 
   // Set GUI according to the current chiller state
   updateDeviceState(model_->getDeviceState());
@@ -56,8 +152,17 @@ void NanotecSMCI36Widget::controlStateChanged(bool enabled)
  */
 void NanotecSMCI36Widget::updateInfo()
 {
-  NQLog("NanotecSMCI36Widget", NQLog::Debug) << "updateInfo()";
+  //NQLog("NanotecSMCI36Widget", NQLog::Debug) << "updateInfo()";
 
   unsigned int status = model_->getStatus();
+  status_->setText(QString::number(status));
+
+  motorID_->setValue(model_->getMotorID());
+
+  controllerSteps_->setText(QString::number(model_->getControllerSteps()));
+  encoderSteps_->setText(QString::number(model_->getEncoderSteps()));
+
+  direction_->setChecked(model_->getDirection());
+  travelDistance_->setValue(model_->getTravelDistance());
 
 }
--- common/NanotecSMCI36Widget.h
+++ common/NanotecSMCI36Widget.h
@@ -2,6 +2,7 @@
 #define NANOTECSMCI36WIDGET_H
 
 #include <QCheckBox>
+#include <QSpinBox>
 #include <QDoubleSpinBox>
 #include <QFormLayout>
 #include <QHBoxLayout>
@@ -12,9 +13,29 @@
 #include <QGroupBox>
 #include <QButtonGroup>
 #include <QRadioButton>
+#include <QLabel>
+#include <QComboBox>
+#include <QPushButton>
 
 #include "NanotecSMCI36Model.h"
 #include "DeviceState.h"
+
+class NanotecSMCI36PositioningModeWidget : public QComboBox
+{
+  Q_OBJECT
+public:
+  explicit NanotecSMCI36PositioningModeWidget(NanotecSMCI36Model* model,
+                                              QWidget *parent = 0);
+
+protected:
+
+  NanotecSMCI36Model * model_;
+
+protected slots:
+
+  void indexChanged(int);
+  void updateInfo();
+};
 
 class NanotecSMCI36Widget : public QWidget
 {
@@ -26,6 +47,20 @@ protected:
   NanotecSMCI36Model* model_;
 
   QCheckBox* smci36CheckBox_;
+
+  QLabel* status_;
+  QSpinBox* motorID_;
+  QLabel* controllerSteps_;
+  QLabel* encoderSteps_;
+
+  NanotecSMCI36PositioningModeWidget* positioningMode_;
+
+  QCheckBox* direction_;
+  QDoubleSpinBox* travelDistance_;
+
+  QPushButton* start_;
+  QPushButton* stop_;
+  QPushButton* quickstop_;
 
 public slots:
 
