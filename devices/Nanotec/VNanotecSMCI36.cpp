59f12be5afd7608bcb49354e885557166a55dc23
Andreas Mussgiller <andreas@mussgiller.de>
2016-09-22 23:30:39 +0200

    hide evil step modes from user

--- devices/Nanotec/VNanotecSMCI36.cpp
+++ devices/Nanotec/VNanotecSMCI36.cpp
@@ -11,8 +11,8 @@ VNanotecSMCI36::VNanotecSMCI36( const ioport_t ioPort )
   stepModeNames_.push_back(std::pair<int,std::string>(smci16MicroStepsPerFullStep, "16 uStep per Full Step"));
   stepModeNames_.push_back(std::pair<int,std::string>(smci32MicroStepsPerFullStep, "32 uStep per Full Step"));
   stepModeNames_.push_back(std::pair<int,std::string>(smci64MicroStepsPerFullStep, "64 uStep per Full Step"));
-  stepModeNames_.push_back(std::pair<int,std::string>(smciFeedRateMode, "Feed Rate Mode"));
-  stepModeNames_.push_back(std::pair<int,std::string>(smciAdaptiveStepMode, "Adaptive Step Mode"));
+  // stepModeNames_.push_back(std::pair<int,std::string>(smciFeedRateMode, "Feed Rate Mode"));
+  // stepModeNames_.push_back(std::pair<int,std::string>(smciAdaptiveStepMode, "Adaptive Step Mode"));
 
   errorCorrectionModeNames_.push_back(std::pair<int,std::string>(smciErrCorrectionOff, "Off"));
   errorCorrectionModeNames_.push_back(std::pair<int,std::string>(smciErrCorrectionAfterTravel, "Correction After Travel"));
