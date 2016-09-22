8e581fe9539f82f399d93dfd98acfbb910a5d4f1
Andreas Mussgiller <andreas@mussgiller.de>
2016-09-22 14:49:53 +0200

    added NanotecSMCI36 model and widget

--- plasma/PlasmaMainWindow.cc
+++ plasma/PlasmaMainWindow.cc
@@ -18,8 +18,17 @@ PlasmaMainWindow::PlasmaMainWindow(QWidget *parent)
 {
     ApplicationConfig* config = ApplicationConfig::instance();
 
+    NQLog("PlasmaMainWindow") << "SMCI36PORT: " << config->getValue("SMCI36PORT").c_str();
+
+    smci36Model_ = new NanotecSMCI36Model(config->getValue("SMCI36PORT").c_str(),
+                                          0.5, 2.5, this);
+
     tabWidget_ = new QTabWidget(this);
     tabWidget_->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+
+    NanotecSMCI36Widget* smci36Widget_ = new NanotecSMCI36Widget(smci36Model_, tabWidget_);
+    smci36Widget_->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+    tabWidget_->addTab(smci36Widget_, "stage");
 
     setCentralWidget(tabWidget_);
     updateGeometry();
@@ -30,4 +39,6 @@ PlasmaMainWindow::PlasmaMainWindow(QWidget *parent)
 void PlasmaMainWindow::quit()
 {
     NQLog("PlasmaMainWindow") << "quit";
+
+    delete smci36Model_;
 }
