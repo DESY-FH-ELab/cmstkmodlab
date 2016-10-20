e25c91983864980ce03cf1eda20cd55603126156
Andreas Mussgiller <andreas@mussgiller.de>
2016-10-20 12:01:40 +0200

    added string constants for device communication

--- devices/Leybold/LeyboldGraphixThree.h
+++ devices/Leybold/LeyboldGraphixThree.h
@@ -30,6 +30,13 @@ class LeyboldGraphixThree : public VLeyboldGraphixThree
 
   LeyboldComHandler* comHandler_;
   bool isDeviceAvailable_;
+
+  static constexpr const char Separator = 0x3B;
+  static constexpr const char EOT       = 0x04;
+  static constexpr const char SI        = 0x0F;
+  static constexpr const char SO        = 0x0E;
+  static constexpr const char ACK       = 0x06;
+  static constexpr const char NACK      = 0x15;
 };
 
 #endif // _LEYBOLDGRAPHIXTHREE_H_
