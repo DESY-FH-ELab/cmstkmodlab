0337d7af2f0d39522fcc153edd925ea74fc2284a
Andreas Mussgiller <andreas@mussgiller.de>
2016-10-21 09:30:58 +0200

    initial version of website added

---
+++ pumpstation/website/ConradSwitch.php
@@ -0,0 +1,27 @@
+﻿<?php
+if (isset($_GET["channel"])) {
+	$channel = strip_tags ($_GET["channel"]);
+	
+	if ((is_numeric($channel)) && ($channel <= 7) && ($channel >= 0) ) {
+		
+		$command = "/Users/mussgill/Sites/PumpStationControl --web getSwitchState ".$channel;
+		exec ($command, $status, $return);
+		
+		if ($status[0] == "0" ) {
+			$status[0] = "1";
+		} else if ($status[0] == "1" ) {
+			$status[0] = "0";
+		}
+		
+		$command = "/Users/mussgill/Sites/PumpStationControl --web setSwitchState ".$channel." ".$status[0];
+		exec ($command, $status, $return);
+		
+		echo($status[0]);
+		
+	} else { 
+		echo("fail");
+	}
+} else {
+	echo("fail");
+}
+?>
