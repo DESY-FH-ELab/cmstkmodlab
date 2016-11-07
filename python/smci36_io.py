cecf655f7d0ad74de003ee4e5b91c8d2f19d5933
Andreas Mussgiller <andreas@mussgiller.de>
2016-11-07 12:07:42 +0100

    changed pins 1&2 to 2&3; removed setting of IO mask

--- python/smci36_io.py
+++ python/smci36_io.py
@@ -3,20 +3,17 @@ import PyTkModLabNanotec
 # connect to device
 smci36 = PyTkModLabNanotec.NanotecSMCI36("/dev/tty.SLAB_USBtoUART")
 
-# set output pin 1 function to user defined
-smci36.SetOutputPinFunction(1, 0)
-
 # set output pin 2 function to user defined
 smci36.SetOutputPinFunction(2, 0)
 
-# set masking of outputs pins 1 and 2 to user defined
-smci36.SetIOMask(0x0004003F)
+# set output pin 3 function to user defined
+smci36.SetOutputPinFunction(3, 0)
 
 # get the state of the IO pins
 smci36.GetIO()
 
-# set output pins 1 and 2 high
-smci36.SetIO(0x00010000 + 0x00020000)
+# set output pins 2 and 3 high
+smci36.SetIO(0x00020000 + 0x00040000)
 
 # get the state of the IO pins
 smci36.GetIO()
