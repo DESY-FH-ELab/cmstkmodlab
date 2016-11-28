b0ae71b543c78b976807c9b3d7f8f8cb5981a809
Andreas Mussgiller <andreas@mussgiller.de>
2016-11-28 17:09:19 +0100

    added setup of vacuum sensors during startup

--- pumpstation/daemon/PumpStationDaemon.cc
+++ pumpstation/daemon/PumpStationDaemon.cc
@@ -63,10 +63,30 @@ int main(int argc, char *argv[])
   std::string leyboldPort = config->getValue("LeyboldPort");
   LeyboldGraphixThreeModel leybold(leyboldPort.c_str(), 5, &app);
 
-  leybold.setDisplayUnit(VLeyboldGraphixThree::DisplayUnit_mbar);
+  if (leybold.getSensorDetectionMode(1)!=VLeyboldGraphixThree::SensorDetectionAuto) {
+    leybold.setSensorDetectionMode(1, VLeyboldGraphixThree::SensorDetectionAuto);
+    if (leybold.getSensorType(1)==VLeyboldGraphixThree::SensorType_TTRx) {
+      leybold.setSensorType(1, VLeyboldGraphixThree::SensorType_TTR91N);
+    }
+  }
+  if (leybold.getSensorDetectionMode(2)!=VLeyboldGraphixThree::SensorDetectionAuto) {
+    leybold.setSensorDetectionMode(2, VLeyboldGraphixThree::SensorDetectionAuto);
+    if (leybold.getSensorType(2)==VLeyboldGraphixThree::SensorType_TTRx) {
+      leybold.setSensorType(2, VLeyboldGraphixThree::SensorType_TTR91N);
+    }
+  }
+  if (leybold.getSensorDetectionMode(3)!=VLeyboldGraphixThree::SensorDetectionAuto) {
+    leybold.setSensorDetectionMode(3, VLeyboldGraphixThree::SensorDetectionAuto);
+    if (leybold.getSensorType(3)==VLeyboldGraphixThree::SensorType_TTRx) {
+      leybold.setSensorType(3, VLeyboldGraphixThree::SensorType_TTR91N);
+    }
+  }
+
   leybold.setSensorName(1, "p1");
   leybold.setSensorName(2, "p2");
   leybold.setSensorName(3, "p3");
+
+  leybold.setDisplayUnit(VLeyboldGraphixThree::DisplayUnit_mbar);
 
   PumpStationModel model(&conrad, &leybold, 5, &app);
 
