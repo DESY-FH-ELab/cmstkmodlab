b3a443f868e870b43b653b010518618fef581638
keaveney <james.keaveney@desy.de>
2016-11-30 16:08:26 +0100

    adding assembly scanner class

---
+++ assembly/assemblyCommon/AssemblyScanner.cc
@@ -0,0 +1,99 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include <QFormLayout>
+#include <QFileDialog>
+#include <QPushButton>
+#include <QString>
+#include <QStringList>
+#include <QPixmap>
+#include <QLabel>
+#include <QApplication>
+
+#include <TGraph.h>
+#include <TCanvas.h>
+
+#include <nqlogger.h>
+#include <ApplicationConfig.h>
+
+#include <QDir>
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+#include <QDesktopServices>
+#else
+#include <QStandardPaths>
+#endif
+
+#include "AssemblyScanner.h"
+
+using namespace std;
+using namespace cv;
+
+AssemblyScanner::AssemblyScanner(AssemblyVUEyeModel *uEyeModel_,LStepExpressModel* lStepExpressModel_)
+{
+    NQLog("AssemblyZScanner::AssemblyScanner()");
+    
+    
+
+    
+}
+
+
+void AssemblyScanner::enable_autofocus(int enabled)
+{
+    NQLog("AssemblyScanner:enable_autofocus() ");
+    
+    if (enabled == 2){
+        NQLog("AssemblyZScanner:enable_autofocus() ") << " connecting motion/vision for scan " ;
+
+    //construct motion manager from motion model
+	// motionManager_ = new LStepExpressMotionManager(lStepExpressModel_);
+    
+    //get mobile camera from camera model
+    //   camera_ = uEyeModel_->getCameraByID(10);
+    // if (camera_){
+    // NQLog("AssemblyZScanner:camera object created() ");
+    // } else{
+    //NQLog("AssemblyZScanner:NULL camera object ");
+    //}
+
+        
+    //connect (this, SIGNAL(getImage()), camera_, SLOT(acquireImage()));
+	//  connect(camera_, SIGNAL(imageAcquired(cv::Mat)),  this, SLOT(write_image(cv::Mat)) );
+
+
+
+    }
+    else if (enabled == 0){
+    
+        NQLog("AssemblyScanner:enable_autofocus() ") << " disconnecting motion/vision for scan "  ;
+        
+        //disconnect (this, SIGNAL(getImage()), camera_, SLOT(acquireImage()));
+        //disconnect(camera_, SIGNAL(imageAcquired(cv::Mat)),  this, SLOT(write_image(cv::Mat)) );
+
+    }
+    
+    
+}
+
+
+void AssemblyScanner::run_scan(double step_size, int nSteps)
+{
+    NQLog("AssemblyScanner:run_scan()");
+    
+}
+
+
+void AssemblyScanner::write_image(cv::Mat)
+{
+    NQLog("AssemblyScanner:write_image()");
+    
+    
+}
+
+
+
+
+
+
---
+++ assembly/assemblyCommon/AssemblyScanner.h
@@ -0,0 +1,54 @@
+#ifndef ASSEMBLYSCANNER_H
+#define ASSEMBLYSCANNER_H
+
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+#include <QWidget>
+#include <QScrollArea>
+#include <QKeyEvent>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QPainter>
+#include <QGroupBox>
+#include <QVBoxLayout>
+#include <QCheckBox>
+#include <QRadioButton>
+
+//vision
+#include <AssemblyUEyeView.h>
+#include "AssemblyVUEyeModel.h"
+#include "AssemblySensorMarkerFinder.h"
+#include "AssemblyVUEyeCamera.h"
+
+//motion
+#include "LStepExpressModel.h"
+#include "LStepExpressMotionManager.h"
+
+
+class AssemblyScanner : public QObject
+{
+  Q_OBJECT
+
+public:
+  AssemblyVUEyeCamera* camera_;
+  LStepExpressMotionManager* motionManager_;
+  explicit AssemblyScanner(AssemblyVUEyeModel *uEyeModel_, LStepExpressModel* lStepExpressModel_);
+
+  double local_range, local_steps, local_delay;
+
+protected:
+
+public slots:
+  void run_scan(double, int);
+  void enable_autofocus(int);
+  void write_image(cv::Mat);
+
+    
+signals:
+
+};
+
+
+#endif // ASSEMBLYAUTOFOCUS_H
