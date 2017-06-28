b01d7e6044af4fb8b61d882dbfc5a2523fa66b09
Andreas Mussgiller <andreas@mussgiller.de>
2017-06-28 07:35:59 +0200

    ignore blocking of switches for the time being

--- pumpstation/daemon/PumpStationModel.cc
+++ pumpstation/daemon/PumpStationModel.cc
@@ -72,7 +72,7 @@ void PumpStationModel::setSwitchBlocked(int channel, bool blocked)
 
 void PumpStationModel::setSwitchEnabled(int channel, bool enabled)
 {
-	if (switchBlocked_[channel]) return;
+	//if (switchBlocked_[channel]) return;
 
   conradModel_->setSwitchEnabled(channel, enabled);
 
