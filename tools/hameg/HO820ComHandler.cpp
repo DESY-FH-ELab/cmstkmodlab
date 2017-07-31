585f553536a60786bac285890db6a371c14775a0
Andreas Mussgiller <andreas@mussgiller.de>
2017-07-31 18:51:25 +0200

    added license statement

--- tools/hameg/HO820ComHandler.cpp
+++ tools/hameg/HO820ComHandler.cpp
@@ -1,3 +1,14 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//               Copyright (C) 2011-2017 - The DESY CMS Group                  //
+//                           All rights reserved                               //
+//                                                                             //
+//      The CMStkModLab source code is licensed under the GNU GPL v3.0.        //
+//      You have the right to modify and/or redistribute this source code      //
+//      under the terms specified in the license, which may be found online    //
+//      at http://www.gnu.org/licenses or at License.txt.                      //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
 
 #include "HO820ComHandler.h"
 
