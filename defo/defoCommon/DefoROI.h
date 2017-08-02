79bde48a7c33df816d7c283d34f7c2b76b2821ad
Andreas Mussgiller <andreas@mussgiller.de>
2017-08-02 11:22:40 +0200

    added license statement

--- defo/defoCommon/DefoROI.h
+++ defo/defoCommon/DefoROI.h
@@ -1,4 +1,16 @@
-#ifndef DEFOROI_H
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//               Copyright (C) 2011-2017 - The DESY CMS Group                  //
+//                           All rights reserved                               //
+//                                                                             //
+//      The CMStkModLab source code is licensed under the GNU GPL v3.0.        //
+//      You have the right to modify and/or redistribute this source code      //
+//      under the terms specified in the license, which may be found online    //
+//      at http://www.gnu.org/licenses or at License.txt.                      //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+s#ifndef DEFOROI_H
 #define DEFOROI_H
 
 #include <QDir>
