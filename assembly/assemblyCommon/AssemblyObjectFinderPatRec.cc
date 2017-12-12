985712458b672b11ad0613c606e65f8c5f1152f2
Marino Missiroli <marino.missiroli@desy.de>
2017-12-12 10:45:27 +0100

    added png for glass-marker; changed default template(1,0) in assemblyCommon/AssemblyObjectFinderPatRec;

--- assembly/assemblyCommon/AssemblyObjectFinderPatRec.cc
+++ assembly/assemblyCommon/AssemblyObjectFinderPatRec.cc
@@ -296,11 +296,8 @@ void AssemblyObjectFinderPatRec::run_PatRec(const int mode_lab, const int mode_o
 //
 //      threshold_tpl_ = 85; // 90 for silicon marker, 88 for glass, 188 for marked-glass
 
-      image_tpl_ = cv::imread(Config::CMSTkModLabBasePath+"/share/assembly/glass_template_raw_cropped.png"   , CV_LOAD_IMAGE_COLOR);
-      threshold_tpl_ = 188; // 90 for silicon marker, 88 for glass, 188 for marked-glass
-
-//      image_tpl_ = cv::imread(Config::CMSTkModLabBasePath+"/share/assembly/RawSensor_3_clipB_temp.png", CV_LOAD_IMAGE_COLOR);
-//      threshold_tpl_ = 50;
+      image_tpl_ = cv::imread(Config::CMSTkModLabBasePath+"/share/assembly/glass_marker1_template.png", CV_LOAD_IMAGE_COLOR);
+      threshold_tpl_ = 165; // 90 for silicon marker, 88 for glass, 165 for marked-glass
     }
     else if(mode_obj == 1)
     {
