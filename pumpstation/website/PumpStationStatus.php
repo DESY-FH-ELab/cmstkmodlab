a2ef5b1f1591c8a78ff8d02a8100ef865c998c98
Andreas Mussgiller <andreas@mussgiller.de>
2018-09-14 18:11:23 +0200

    test version of pump station station json creator

---
+++ pumpstation/website/PumpStationStatus.php
@@ -0,0 +1,17 @@
+<?php
+
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//               Copyright (C) 2011-2018 - The DESY CMS Group                  //
+//                           All rights reserved                               //
+//                                                                             //
+//      The CMStkModLab source code is licensed under the GNU GPL v3.0.        //
+//      You have the right to modify and/or redistribute this source code      //
+//      under the terms specified in the license, which may be found online    //
+//      at http://www.gnu.org/licenses or at License.txt.                      //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+echo ("PumpStationStatus");
+
+?>
