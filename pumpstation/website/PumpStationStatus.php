c0d665fd16001c009d261385b22334050af614e1
Andreas Mussgiller <andreas@mussgiller.de>
2018-09-14 18:24:22 +0200

    test of version with switch status

--- pumpstation/website/PumpStationStatus.php
+++ pumpstation/website/PumpStationStatus.php
@@ -12,6 +12,20 @@
 //                                                                             //
 /////////////////////////////////////////////////////////////////////////////////
 
-echo ("PumpStationStatus");
+$ini_array = parse_ini_file ( "pumpstation.ini" );
 
+$command = $ini_array ['DocumentRoot'] . "/PumpStationControl --web getSwitchBlockedStatus";
+exec ( $command, $result, $return );
+$SB = split(';', $result);
+
+$command = $ini_array ['DocumentRoot'] . "/PumpStationControl --web getSwitchStatus";
+exec ( $command, $result, $return );
+$SS = split(';', $result);
+
+json = array(
+	'SB0' => $SB[0], 'SB1' => $SB[1], 'SB2' => $SB[2], 'SB3' => $SB[3], 'SB4' => $SB[4], 
+	'SS0' => $SS[0], 'SS1' => $SS[1], 'SS2' => $SS[2], 'SS3' => $SS[3], 'SS4' => $SS[4], 
+);
+
+echo (json_encode($json));
 ?>
