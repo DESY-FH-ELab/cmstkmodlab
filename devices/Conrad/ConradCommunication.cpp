f664ee19cf9d52795662a310672968eb95026d3a
Marino Missiroli <marino.missiroli@desy.de>
2019-06-14 08:54:27 +0200

    [devices] updated fprintf calls in Conrad/ConradCommunication.cpp

--- devices/Conrad/ConradCommunication.cpp
+++ devices/Conrad/ConradCommunication.cpp
@@ -115,7 +115,7 @@ bool ConradCommunication::sendCommand(unsigned char command, unsigned char addre
   tcdrain(m_ioPort);
 
   if (wroteBytes != 4) {
-    fprintf(stderr, "[Conrad] ERROR: Could send only %d of 4 bytes\n", wroteBytes);
+    fprintf(stderr, "[Conrad] ERROR (port: %s): Could send only %d of 4 bytes\n", comPort().c_str(), wroteBytes);
     return false;
   }
 
@@ -132,18 +132,18 @@ bool ConradCommunication::receiveAnswer(unsigned char* answer, unsigned char* ad
 
   int readBytes = read(m_ioPort, buffer, 4);
   if (readBytes == -1) {
-    fprintf(stderr, "[Conrad] ERROR: Reading from card failed\n");
+    fprintf(stderr, "[Conrad] ERROR (port: %s): Reading from card failed\n", comPort().c_str());
     return false;
   }
 
   if (readBytes != 4) {
-    fprintf(stderr, "[Conrad] ERROR: Could receive only %d of 4 bytes\n", readBytes);
+    fprintf(stderr, "[Conrad] ERROR (port: %s): Could receive only %d of 4 bytes\n", comPort().c_str(), readBytes);
     return false;
   }
 
   int checksum = buffer[0] ^ buffer[1] ^ buffer[2];
   if (checksum != buffer[3]) {
-    fprintf(stderr, "[Conrad] ERROR: XOR checksum is %x but should be %x\n", checksum, buffer[3]);
+    fprintf(stderr, "[Conrad] ERROR (port: %s): XOR checksum is %x but should be %x\n", comPort().c_str(), checksum, buffer[3]);
     return false;
   }
 
