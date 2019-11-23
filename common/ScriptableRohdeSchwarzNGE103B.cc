ef10d657827bfcf8331bc4dcfbc207f22ff42922
Andreas Mussgiller <andreas@mussgiller.de>
2019-11-23 16:51:02 +0100

    made Rohde&Schwarz power supply scriptable

---
+++ common/ScriptableRohdeSchwarzNGE103B.cc
@@ -0,0 +1,123 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//               Copyright (C) 2011-2019 - The DESY CMS Group                  //
+//                           All rights reserved                               //
+//                                                                             //
+//      The CMStkModLab source code is licensed under the GNU GPL v3.0.        //
+//      You have the right to modify and/or redistribute this source code      //
+//      under the terms specified in the license, which may be found online    //
+//      at http://www.gnu.org/licenses or at License.txt.                      //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+#include <QMutexLocker>
+
+#include "ScriptableRohdeSchwarzNGE103B.h"
+
+ScriptableRohdeSchwarzNGE103B::ScriptableRohdeSchwarzNGE103B(RohdeSchwarzNGE103BModel* model,
+                                                           QObject *parent) :
+    QObject(parent),
+    model_(model)
+{
+  connect(this, SIGNAL(changeOutputState(int, bool)),
+          model_, SLOT(setOutputState(int, bool)));
+  connect(this, SIGNAL(changeSetVoltage(int, float)),
+          model_, SLOT(setVoltage(int, float)));
+  connect(this, SIGNAL(changeSetCurrent(int, float)),
+          model_, SLOT(setCurrent(int, float)));
+  connect(this, SIGNAL(changeEasyRampDuration(int, float)),
+          model_, SLOT(setEasyRampDuration(int, float)));
+  connect(this, SIGNAL(changeEasyRampState(int, bool)),
+          model_, SLOT(setEasyRampState(int, bool)));
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getOutputState(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  bool value = model_->getOutputState(channel);
+  return QScriptValue(value);
+}
+
+void ScriptableRohdeSchwarzNGE103B::setOutputState(int channel, bool state)
+{
+  if (channel<1 || channel>3) return;
+  QMutexLocker locker(&mutex_);
+  emit changeOutputState(channel, state);
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getVoltage(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  if (channel<1 || channel>3) return QScriptValue(-1);
+  bool value = model_->getVoltage(channel);
+  return QScriptValue(value);
+}
+
+void ScriptableRohdeSchwarzNGE103B::setVoltage(int channel, float voltage)
+{
+  if (channel<1 || channel>3 || voltage<0) return;
+  QMutexLocker locker(&mutex_);
+  emit changeSetVoltage(channel, voltage);
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getMeasuredVoltage(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  if (channel<1 || channel>3) return QScriptValue(-1);
+  bool value = model_->getMeasuredVoltage(channel);
+  return QScriptValue(value);
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getCurrent(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  if (channel<1 || channel>3) return QScriptValue(-1);
+  bool value = model_->getCurrent(channel);
+  return QScriptValue(value);
+}
+
+void ScriptableRohdeSchwarzNGE103B::setCurrent(int channel, float current)
+{
+  if (channel<1 || channel>3 || current<0) return;
+  QMutexLocker locker(&mutex_);
+  emit changeSetCurrent(channel, current);
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getMeasuredCurrent(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  if (channel<1 || channel>3) return QScriptValue(-1);
+  bool value = model_->getMeasuredCurrent(channel);
+  return QScriptValue(value);
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getEasyRampDuration(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  if (channel<1 || channel>3) return QScriptValue(-1);
+  bool value = model_->getEasyRampDuration(channel);
+  return QScriptValue(value);
+}
+
+void ScriptableRohdeSchwarzNGE103B::setEasyRampDuration(int channel, float duration)
+{
+  if (channel<1 || channel>3 || duration<0) return;
+  QMutexLocker locker(&mutex_);
+  emit changeEasyRampDuration(channel, duration);
+}
+
+QScriptValue ScriptableRohdeSchwarzNGE103B::getEasyRampState(int channel)
+{
+  QMutexLocker locker(&mutex_);
+  bool value = model_->getEasyRampState(channel);
+  return QScriptValue(value);
+}
+
+void ScriptableRohdeSchwarzNGE103B::setEasyRampState(int channel, bool state)
+{
+  if (channel<1 || channel>3) return;
+  QMutexLocker locker(&mutex_);
+  emit changeEasyRampState(channel, state);
+}
---
+++ common/ScriptableRohdeSchwarzNGE103B.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                                                                             //
+//               Copyright (C) 2011-2019 - The DESY CMS Group                  //
+//                           All rights reserved                               //
+//                                                                             //
+//      The CMStkModLab source code is licensed under the GNU GPL v3.0.        //
+//      You have the right to modify and/or redistribute this source code      //
+//      under the terms specified in the license, which may be found online    //
+//      at http://www.gnu.org/licenses or at License.txt.                      //
+//                                                                             //
+/////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SCRIPTABLEROHDESCHWARZNGE103B_H
+#define SCRIPTABLEROHDESCHWARZNGE103B_H
+
+#include <QObject>
+#include <QMutex>
+#include <QScriptValue>
+
+#include <RohdeSchwarzNGE103BModel.h>
+
+class ScriptableRohdeSchwarzNGE103B : public QObject
+{
+  Q_OBJECT
+public:
+  explicit ScriptableRohdeSchwarzNGE103B(RohdeSchwarzNGE103BModel* model, QObject *parent = 0);
+
+public slots:
+
+  QScriptValue getOutputState(int channel);
+  void setOutputState(int channel, bool state);
+
+  QScriptValue getVoltage(int channel);
+  void setVoltage(int channel, float voltage);
+  QScriptValue getMeasuredVoltage(int channel);
+
+  QScriptValue getCurrent(int channel);
+  void setCurrent(int channel, float current);
+  QScriptValue getMeasuredCurrent(int channel);
+
+  QScriptValue getEasyRampDuration(int channel);
+  void setEasyRampDuration(int channel, float voltage);
+
+  QScriptValue getEasyRampState(int channel);
+  void setEasyRampState(int channel, bool state);
+
+signals:
+
+  void changeOutputState(int, bool);
+  void changeSetVoltage(int, float);
+  void changeSetCurrent(int, float);
+  void changeEasyRampDuration(int, float);
+  void changeEasyRampState(int, bool);
+
+protected:
+
+  RohdeSchwarzNGE103BModel* model_;
+
+  // For thread safety
+  QMutex mutex_;
+};
+
+#endif // SCRIPTABLEROHDESCHWARZNGE103B_H
