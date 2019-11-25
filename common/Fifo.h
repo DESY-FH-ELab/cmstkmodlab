b8ca05f3a43c6b23419b31d958117cd9e0671f9d
Andreas Mussgiller <andreas@mussgiller.de>
2019-11-25 09:12:24 +0100

    beautified indentation

--- common/Fifo.h
+++ common/Fifo.h
@@ -21,84 +21,84 @@ template <class T> class Fifo
 {
 public:
 
-    typedef T      value_type;
+  typedef T value_type;
 
-    Fifo(int N) {
-    	buffer_.resize(N);
-    	size_ = 0;
-    	capacity_ = N;
-    	currentFirstIndex_ = 0;
-    }
+  Fifo(int N) {
+    buffer_.resize(N);
+    size_ = 0;
+    capacity_ = N;
+    currentFirstIndex_ = 0;
+  }
 
-    void resize(int N) {
-        buffer_.resize(N);
-        capacity_ = N;
-    }
+  void resize(int N) {
+    buffer_.resize(N);
+    capacity_ = N;
+  }
 
-    int capacity() const {
-        return capacity_;
-    }
+  int capacity() const {
+    return capacity_;
+  }
 
-    int size() const {
-    	return size_;
-    }
+  int size() const {
+    return size_;
+  }
 
-    virtual void push(const T& value) {
-    	if (size_<capacity_) {
-    		buffer_[size_] = value;
-    		size_++;
-    		currentFirstIndex_ = size_ - 1;
-    	} else {
-    		currentFirstIndex_++;
-    		if (currentFirstIndex_>=capacity_) currentFirstIndex_ = 0;
-    		buffer_[currentFirstIndex_] = value;
-    	}
+  virtual void push(const T& value) {
+    if (size_<capacity_) {
+      buffer_[size_] = value;
+      size_++;
+      currentFirstIndex_ = size_ - 1;
+    } else {
+      currentFirstIndex_++;
+      if (currentFirstIndex_>=capacity_) currentFirstIndex_ = 0;
+      buffer_[currentFirstIndex_] = value;
     }
+  }
 
-    const T & at(int i) const {
-    	if (i>=size_) return last();
-    	int idx = currentFirstIndex_ - i;
-    	if (idx<0) idx += size_;
-    	return buffer_[idx];
-    }
+  const T & at(int i) const {
+    if (i>=size_) return last();
+    int idx = currentFirstIndex_ - i;
+    if (idx<0) idx += size_;
+    return buffer_[idx];
+  }
 
-    T & first() {
-    	return buffer_[currentFirstIndex_];
-    }
+  T & first() {
+    return buffer_[currentFirstIndex_];
+  }
 
-    const T & first() const {
-    	return buffer_[currentFirstIndex_];
-    }
+  const T & first() const {
+    return buffer_[currentFirstIndex_];
+  }
 
-    T & last() {
-    	int idx = currentFirstIndex_;
-    	if (size_<capacity_) {
-    		idx = 0;
-    	} else {
-    		idx++;
-    		if (idx>=capacity_) idx = 0;
-    	}
-    	return buffer_[idx];
+  T & last() {
+    int idx = currentFirstIndex_;
+    if (size_<capacity_) {
+      idx = 0;
+    } else {
+      idx++;
+      if (idx>=capacity_) idx = 0;
     }
+    return buffer_[idx];
+  }
 
-    const T & last() const {
-    	int idx = currentFirstIndex_;
-    	if (size_<capacity_) {
-    		idx = 0;
-    	} else {
-    		idx++;
-    		if (idx>=capacity_) idx = 0;
-    	}
-    	return buffer_[idx];
+  const T & last() const {
+    int idx = currentFirstIndex_;
+    if (size_<capacity_) {
+      idx = 0;
+    } else {
+      idx++;
+      if (idx>=capacity_) idx = 0;
     }
+    return buffer_[idx];
+  }
 
 protected:
 
-    int size_;
-    int capacity_;
-    int currentFirstIndex_;
+  int size_;
+  int capacity_;
+  int currentFirstIndex_;
 
-    QVector<T> buffer_;
+  QVector<T> buffer_;
 };
 
 #endif // RINGBUFFER_H
