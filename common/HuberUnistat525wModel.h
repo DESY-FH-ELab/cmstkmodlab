fda41e109c931e3bf588fc92383a71645de0e48f
Andreas Mussgiller <andreas.mussgiller@cern.ch>
2021-06-02 17:03:47 +0200

    removed fake device as the default

--- common/HuberUnistat525wModel.h
+++ common/HuberUnistat525wModel.h
@@ -21,7 +21,6 @@
 #include "DeviceState.h"
 #include "DeviceParameter.h"
 
-/*
 #ifdef USE_FAKEIO
 #include "devices/Huber/HuberPilotOneFake.h"
 typedef HuberPilotOneFake HuberPilotOne_t;
@@ -29,9 +28,6 @@ typedef HuberPilotOneFake HuberPilotOne_t;
 #include "devices/Huber/HuberPilotOne.h"
 typedef HuberPilotOne HuberPilotOne_t;
 #endif
-*/
-#include "devices/Huber/HuberPilotOneFake.h"
-typedef HuberPilotOneFake HuberPilotOne_t;
 
 /**
   Command and control model of the Huber Unistat 525w chiller.
