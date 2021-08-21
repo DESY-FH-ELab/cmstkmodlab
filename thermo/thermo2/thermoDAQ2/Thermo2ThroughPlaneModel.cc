e8aeb2d7ed1d158e979ccdb2eeddccaca9f895d5
Andreas Mussgiller <andreas.mussgiller@cern.ch>
2021-08-21 10:54:56 +0200

    fixed code indentation

--- thermo/thermo2/thermoDAQ2/Thermo2ThroughPlaneModel.cc
+++ thermo/thermo2/thermoDAQ2/Thermo2ThroughPlaneModel.cc
@@ -178,13 +178,13 @@ void Thermo2ThroughPlaneModel::keithleyInfoChanged()
   changed |= updateIfChanged<bool>(keithleyState_, keithleyModel_->getDeviceState()==READY ? true : false);
   for (unsigned int i=0;i<6;++i) {
     changed |= updateIfChanged<bool>(keithleyTopSensorStates_[i],
-				     keithleyModel_->getSensorState(keithleyTopSensors_[i])==READY ? true : false);
+        keithleyModel_->getSensorState(keithleyTopSensors_[i])==READY ? true : false);
     changed |= updateIfChanged<double>(keithleyTopTemperatures_[i],
         keithleyModel_->getTemperature(keithleyTopSensors_[i]) + keithleyTopOffsets_[i]);
     if (keithleyTopSensorStates_[i]) countTop++;
 
     changed |= updateIfChanged<bool>(keithleyBottomSensorStates_[i],
-				     keithleyModel_->getSensorState(keithleyBottomSensors_[i])==READY ? true : false);
+        keithleyModel_->getSensorState(keithleyBottomSensors_[i])==READY ? true : false);
     changed |= updateIfChanged<double>(keithleyBottomTemperatures_[i],
         keithleyModel_->getTemperature(keithleyBottomSensors_[i]) + keithleyBottomOffsets_[i]);
     if (keithleyBottomSensorStates_[i]) countBottom++;
@@ -201,7 +201,7 @@ void Thermo2ThroughPlaneModel::keithleyInfoChanged()
       unsigned int p;
       double pos;
       int ret;
-      
+
       if (countTop<4) {
         p = 2;
       } else {
@@ -233,27 +233,27 @@ void Thermo2ThroughPlaneModel::keithleyInfoChanged()
       gsl_multifit_robust_free(work);
 
       if (ret!=GSL_EMAXITER) {
-	sampleTTop_ = gsl_vector_get(c, 0);
-	gradientTop_ = -1.0 * gsl_vector_get(c, 1) * 1000.;
-	powerTop_ = gradientTop_ * kBlock_ * ABlock_ * 1e-6;
+        sampleTTop_ = gsl_vector_get(c, 0);
+        gradientTop_ = -1.0 * gsl_vector_get(c, 1) * 1000.;
+        powerTop_ = gradientTop_ * kBlock_ * ABlock_ * 1e-6;
 
-	NQLogDebug("Thermo2ThroughPlaneModel") << "gradientTop_ = " << gradientTop_;
-	NQLogDebug("Thermo2ThroughPlaneModel") << "powerTop_ = " << powerTop_;
+        NQLogDebug("Thermo2ThroughPlaneModel") << "gradientTop_ = " << gradientTop_;
+        NQLogDebug("Thermo2ThroughPlaneModel") << "powerTop_ = " << powerTop_;
       }
-      
+
       gsl_matrix_free(X);
       gsl_vector_free(x);
       gsl_vector_free(y);
       gsl_vector_free(c);
       gsl_matrix_free(cov);
-      
+
       if (countBottom<4) {
-	p = 2;
+        p = 2;
       } else {
         p = 3;
       }
       p = 2; // make linear fit the default
-      
+
       X = gsl_matrix_alloc(countBottom, p);
       x = gsl_vector_alloc(countBottom);
       y = gsl_vector_alloc(countBottom);
@@ -278,14 +278,14 @@ void Thermo2ThroughPlaneModel::keithleyInfoChanged()
       gsl_multifit_robust_free(work);
 
       if (ret!=GSL_EMAXITER) {
-	sampleTBottom_ = gsl_vector_get(c, 0);
-	gradientBottom_ = gsl_vector_get(c, 1) * 1000.;
-	powerBottom_ = gradientBottom_ * kBlock_ * ABlock_ * 1e-6;
+        sampleTBottom_ = gsl_vector_get(c, 0);
+        gradientBottom_ = gsl_vector_get(c, 1) * 1000.;
+        powerBottom_ = gradientBottom_ * kBlock_ * ABlock_ * 1e-6;
 
-	NQLogDebug("Thermo2ThroughPlaneModel") << "gradientBottom_ = " << gradientBottom_;
-	NQLogDebug("Thermo2ThroughPlaneModel") << "powerBottom_ = " << powerBottom_;
+        NQLogDebug("Thermo2ThroughPlaneModel") << "gradientBottom_ = " << gradientBottom_;
+        NQLogDebug("Thermo2ThroughPlaneModel") << "powerBottom_ = " << powerBottom_;
       }
-      
+
       gsl_matrix_free(X);
       gsl_vector_free(x);
       gsl_vector_free(y);
@@ -293,7 +293,7 @@ void Thermo2ThroughPlaneModel::keithleyInfoChanged()
       gsl_matrix_free(cov);
 
       sampleTMiddle_ = 0.5*(sampleTTop_ + sampleTBottom_);
-      
+
     } else {
       calculationState_ = false;
     }
