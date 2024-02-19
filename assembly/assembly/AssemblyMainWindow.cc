57f5ff0fe232f01b45c3ea5c35475d3103a7ef5b
Paul Schuetze <paul.schuetze@desy.de>
2024-02-19 13:32:06 +0100

    QMessageBox has to be issued from the main thread. Emit signal in motion manager and display in main window

--- assembly/assembly/AssemblyMainWindow.cc
+++ assembly/assembly/AssemblyMainWindow.cc
@@ -1346,6 +1346,13 @@ void AssemblyMainWindow::update_vacuum_information(const int channel, const Swit
     to_be_updated->setStyleSheet("QLabel { color : gray ; font: bold }");
     to_be_updated->setAlignment(Qt::AlignCenter);
   }
+  
+void AssemblyMainWindow::warn_on_stage_limits(const double target_pos, const char axis, const double limit_pos_lower, const double limit_pos_upper)
+{
+    QMessageBox::warning(0, QString("[AssemblyMainWindow]"),
+    QString("Attempting to move %1-axis to %2 . This is beyond the limit of this stage: [ %3 , %4 ]").arg(axis).arg(target_pos).arg(limit_pos_lower).arg(limit_pos_upper),
+        QMessageBox::Ok
+    );
 }
 
 void AssemblyMainWindow::warn_on_stage_limits(const double target_pos, const char axis, const double limit_pos_lower, const double limit_pos_upper)
