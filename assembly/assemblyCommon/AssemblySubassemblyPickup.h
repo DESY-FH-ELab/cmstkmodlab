16ca4a2b69e69ea361b15885857080419a9e2b63
Paul Schuetze <paul.schuetze@desy.de>
2024-10-25 11:20:30 +0200

    Subassembly pickup: adjust sequence to account for the pickup jig

--- assembly/assemblyCommon/AssemblySubassemblyPickup.h
+++ assembly/assemblyCommon/AssemblySubassemblyPickup.h
@@ -44,6 +44,7 @@ class AssemblySubassemblyPickup : public QObject
   int vacuum_pickup_;
   int vacuum_spacer_;
   int vacuum_basepl_;
+  int vacuum_sub_;
 
   bool use_smartMove_;
   bool in_action_;
@@ -59,11 +60,14 @@ class AssemblySubassemblyPickup : public QObject
 
   void use_smartMove(const int);
 
+  void EnableVacuumSpacers_start();
+  void EnableVacuumSpacers_finish();
+
   void GoToSensorMarkerPreAlignment_start();
   void GoToSensorMarkerPreAlignment_finish();
 
-  void EnableVacuumSpacers_start();
-  void EnableVacuumSpacers_finish();
+  void EnableVacuumSubassembly_start();
+  void EnableVacuumSubassembly_finish();
 
   void switchToAlignmentTab_PSS();
 
@@ -76,11 +80,14 @@ class AssemblySubassemblyPickup : public QObject
   void LowerPickupToolOntoSubassembly_start();
   void LowerPickupToolOntoSubassembly_finish();
 
-  void DisableVacuumSpacers_start();
-  void DisableVacuumSpacers_finish();
+  void DisableVacuumSubassembly_start();
+  void DisableVacuumSubassembly_finish();
 
   void PickupSubassembly_start();
   void PickupSubassembly_finish();
+
+  void DisableVacuumSpacers_start();
+  void DisableVacuumSpacers_finish();
 
  signals:
 
@@ -101,6 +108,9 @@ class AssemblySubassemblyPickup : public QObject
 
   void EnableVacuumSpacers_finished();
   void DisableVacuumSpacers_finished();
+
+  void EnableVacuumSubassembly_finished();
+  void DisableVacuumSubassembly_finished();
 
   void switchToAlignmentTab_PSS_request();
 
